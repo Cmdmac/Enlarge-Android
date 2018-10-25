bf036f45c7cc5b09ed3586261433298b5144e263
cmdmac <cmdmac@126.com>
2018-10-25 10:37:25 +0800

    remove no use license, refact IRouter pkg path

--- app/src/main/java/org/cmdmac/enlarge/server/serverlets/RouterNanoHTTPD.java
+++ app/src/main/java/org/cmdmac/enlarge/server/serverlets/RouterNanoHTTPD.java
@@ -1,41 +1,5 @@
 package org.cmdmac.enlarge.server.serverlets;
 
-/*
- * #%L
- * NanoHttpd-Samples
- * %%
- * Copyright (C) 2012 - 2015 nanohttpd
- * %%
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
- * 3. Neither the name of the nanohttpd nor the names of its contributors
- *    may be used to endorse or promote products derived from this software without
- *    specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- * #L%
- */
-
-import java.io.File;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -44,19 +8,14 @@ import java.util.logging.Logger;
 
 //import org.cmdmac.enlarge.server.ControllerInject;
 import org.cmdmac.enlarge.server.ControllerInject;
-import org.cmdmac.enlarge.server.annotations.Controller;
-import org.cmdmac.enlarge.server.annotations.Param;
-import org.cmdmac.enlarge.server.annotations.RequestMapping;
 import org.cmdmac.enlarge.server.handlers.DefaultHandler;
 import org.cmdmac.enlarge.server.handlers.StaticPageHandler;
-import org.cmdmac.enlarge.server.processor.IRouter;
 import org.nanohttpd.protocols.http.IHTTPSession;
 import org.nanohttpd.protocols.http.response.Response;
 import org.nanohttpd.protocols.websockets.NanoWSD;
 
 /**
- * @author vnnv
- * @author ritchieGitHub
+ * @author cmdmac
  */
 public abstract class RouterNanoHTTPD extends NanoWSD {
 
